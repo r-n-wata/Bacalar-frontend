b6d486d0912ea8bf649b8ed87d8242b623d8776f
Ruth Wata <88207822+r-n-wata@users.noreply.github.com>
2026-07-26 14:00:50 -0500

    Fix fast refresh linting for lazy route pages

--- src/app/router/appRoutes.tsx
+++ src/app/router/appRoutes.tsx
@@ -1,71 +1,25 @@
-import { lazy, Suspense, type ReactNode } from 'react'
+import { Suspense, type ReactNode } from 'react'
 import { AppShell } from '../../components/templates/AppShell'
 import { LoadingSpinner } from '../../components/atoms/LoadingSpinner'
 import { ProtectedAdminRoute } from '../../features/admin/pages/ProtectedAdminRoute'
 import { PublicRouteStatusPage } from './PublicRouteStatusPage'
-
-const HomePage = lazy(async () => ({
-  default: (await import('../../features/home/pages/HomePage')).HomePage,
-}))
-const EventsPage = lazy(async () => ({
-  default: (await import('../../features/events/pages/EventsPage')).EventsPage,
-}))
-const EventSubmissionPage = lazy(async () => ({
-  default: (await import('../../features/events/pages/EventSubmissionPage'))
-    .EventSubmissionPage,
-}))
-const EventDetailPage = lazy(async () => ({
-  default: (await import('../../features/events/pages/EventDetailPage'))
-    .EventDetailPage,
-}))
-const AdminLoginPage = lazy(async () => ({
-  default: (await import('../../features/admin/pages/AdminLoginPage'))
-    .AdminLoginPage,
-}))
-const AdminDashboardPage = lazy(async () => ({
-  default: (await import('../../features/admin/pages/AdminDashboardPage'))
-    .AdminDashboardPage,
-}))
-const AdminPublishedContentPage = lazy(async () => ({
-  default: (
-    await import('../../features/admin/pages/AdminPublishedContentPage')
-  ).AdminPublishedContentPage,
-}))
-const AdminPublishedContentEditPage = lazy(async () => ({
-  default: (
-    await import('../../features/admin/pages/AdminPublishedContentEditPage')
-  ).AdminPublishedContentEditPage,
-}))
-const AdminSubmissionDetailPage = lazy(async () => ({
-  default: (
-    await import('../../features/admin/pages/AdminSubmissionDetailPage')
-  ).AdminSubmissionDetailPage,
-}))
-const RestaurantsPage = lazy(async () => ({
-  default: (await import('../../features/restaurants/pages/RestaurantsPage'))
-    .RestaurantsPage,
-}))
-const RestaurantSubmissionPage = lazy(async () => ({
-  default: (
-    await import('../../features/restaurants/pages/RestaurantSubmissionPage')
-  ).RestaurantSubmissionPage,
-}))
-const RestaurantDetailPage = lazy(async () => ({
-  default: (
-    await import('../../features/restaurants/pages/RestaurantDetailPage')
-  ).RestaurantDetailPage,
-}))
-const ToursPage = lazy(async () => ({
-  default: (await import('../../features/tours/pages/ToursPage')).ToursPage,
-}))
-const TourSubmissionPage = lazy(async () => ({
-  default: (await import('../../features/tours/pages/TourSubmissionPage'))
-    .TourSubmissionPage,
-}))
-const TourDetailPage = lazy(async () => ({
-  default: (await import('../../features/tours/pages/TourDetailPage'))
-    .TourDetailPage,
-}))
+import {
+  AdminDashboardPage,
+  AdminLoginPage,
+  AdminPublishedContentEditPage,
+  AdminPublishedContentPage,
+  AdminSubmissionDetailPage,
+  EventDetailPage,
+  EventsPage,
+  EventSubmissionPage,
+  HomePage,
+  RestaurantDetailPage,
+  RestaurantsPage,
+  RestaurantSubmissionPage,
+  TourDetailPage,
+  ToursPage,
+  TourSubmissionPage,
+} from './LazyRoutePages'
 
 function withSuspense(element: ReactNode) {
   return (
